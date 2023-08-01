58e5dec755e542132f2d7b3e6f81d5358c397d69
ibadurrehman718 <ibadurrehman718@gmail.com>
2023-08-02 03:47:42 +0500

    Added

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get marks from user input
+var ChemistryMarks = +prompt("Enter the marks obtained in Chemistry:")
+var PhysicsMarks = +prompt("Enter the marks obtained in Physics:")
+var MathsMarks = +prompt("Enter the marks obtained in Maths:")
+var EnglishMarks = +prompt("Enter the marks obtained in English:")
+var UrduMarks = +prompt("Enter the marks obtained in Urdu:")
+var ComputerMarks= +prompt("Enter the marks obtained in Computer:")
+
+
+var totalMarks = 600;
+var MarksObtained = ChemistryMarks + PhysicsMarks + MathsMarks + EnglishMarks + UrduMarks +ComputerMarks
+var percentage = ( MarksObtained / totalMarks ) * 100
+
+alert(`Total Marks: ${totalMarks}\nMarks Obtained: ${MarksObtained}\nPercentage: ${percentage}%`)
